a3fe3dd82d38c25f998b73e9bbf6f9d37e9871dc
ongtiera <164710955+ongtiera@users.noreply.github.com>
2024-03-24 11:21:29 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}'https://eth1.lava.build/lava-referer-35f324ff-161f-4dc1-8a5a-e604dc741e34/)
 
 
 echo "$curl_output" >> Update.tsx
